c4eefdca8af29477e8087fddeddc2a60758a79c5
Zahid Laguna <zlaguna316@gmail.com>
2023-10-19 19:31:52 -0500

    replaced code with previous version

--- lab8/ajax.php
+++ lab8/ajax.php
@@ -1,3 +1,6 @@
 <?php
 
 echo "Hello ".$_GET[fname]." ".$_GET[lname]."!";
+
+?>
+```
--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,4 +1,20 @@
-
 function getDataFromForm() {
-  alert("it worked!");
+  
+  var firstname=document.getElementById('fname').value;
+  var lastname=document.getElementById('lname').value;
+  runAjax(firstname,lastname);
+}
+
+function runAjax(fname ,lname) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    //Edit this
+  const stringResponse=xhttp.responseText;
+  var string = fname+" "+lname;
+   document.getElementById("responseString").innerHTML = stringResponse;
+
+    //console.log(this.responseText);
+    }
+    xhttp.open("GET", `./ajax.php?fname=${fname}&lname=${lname}`, true);
+    xhttp.send();
 }
