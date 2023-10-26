8dfe5bb6d2c4090a3da3813a9af8167ac4032340
Zahid Laguna <zlaguna316@gmail.com>
2023-10-26 11:52:51 -0500

    deleted some comments

--- Lab10/savedata.php
+++ Lab10/savedata.php
@@ -1,23 +1,22 @@
 <?php
-// save_data.php
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstName = $_POST["first_name"];
     $lastName = $_POST["last_name"];
 
-    // File path in a non-public folder
+   
     $filePath = "/home/zahid/webtechsavedata/data.txt";
 
-    // Open the file in append mode
+
     $file = fopen($filePath, "a");
 
-    // Write the data to the file
+  
     fwrite($file, $firstName . " " . $lastName . PHP_EOL);
 
-    // Close the file
+
     fclose($file);
 
-    // Redirect back to the form page
     header("Location: index.html");
     exit();
 }
